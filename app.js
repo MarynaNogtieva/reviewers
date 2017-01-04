9250779ab02b2c215c4c96ae6b6089f1d0c569ef
MarynaNogtieva <marinanogteva@gmail.com>
2017-01-04 11:19:38 -0500

    Moved javascript code from index.html to app.js

---
+++ app.js
@@ -0,0 +1,70 @@
+$(function() {
+
+
+var data, data2;
+var selected = $("#select1").selectize({
+	delimiter:',',
+	
+	options: [{id:1, title:'Sam'},{id:2, title:'Alice'}],
+	valueField:'id',
+	labelField:'title',
+	searchField: 'title',
+	sortField:'title',
+	// onChange: function(value){
+	// 	data = value;
+	// }
+	
+});
+var selectedApprovers = $("#select2").selectize({
+	delimiter:',',
+	
+	options: [{id:93, title:'Bill'},{id:21, title:'Julie'},{id:23, title:'Alex'}],
+	valueField:'id',
+	labelField:'title',
+	searchField: 'title',
+	sortField:'title',
+	// onChange: function(value){
+	// 	data = value;
+	// }
+	
+});
+
+selected;
+selectedApprovers;
+var selectizeReviewers =selected[0].selectize;
+var selectizeApprovers = selectedApprovers[0].selectize;
+//data = $("#select1").data("selected",selected );
+data=selected[0].selectize.options;
+data2=selectedApprovers[0].selectize.options;
+// var selectedNumber = $.map(data,function(value,index){
+// 		return [value];
+// 	})	
+var arrSelectedRevievers=[];
+var arrSelectedApprovers=[];
+selectizeReviewers.on('change',function(){
+	var current=selectizeReviewers.getValue();
+	for(var i =0; i < current.length; i++){
+		arrSelectedRevievers[i] = selectizeReviewers.options[ current[i] ];
+	}
+  
+	//mydata = selectize.options[ current ];
+ })
+
+selectizeApprovers.on('change',function(){
+	//data2=selectizeApprovers.getValue();
+	var current=selectizeApprovers.getValue();
+	for(var i =0; i < current.length; i++){
+		arrSelectedApprovers[i] = selectizeApprovers.options[ current[i] ];
+	}
+	
+})
+
+
+
+$("#btnSendForReview").on('click',function(){
+
+  //alert(selectedNumber);
+  console.log(arrSelectedRevievers,arrSelectedApprovers);
+
+ });
+  });
